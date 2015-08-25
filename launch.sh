1624921bd5e6e970abd2c8770c3f7ac16c9afebb
James C Russell <james@101ideas.cz>
2015-08-25 14:53:32 +0100

    Imported some things from tpires/neo4j and rewritten HOSTNAME -> 0.0.0.0

--- launch.sh
+++ launch.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 NEO4J_HOME=/var/lib/neo4j
 
@@ -12,7 +13,7 @@ if [ -n "$NEO4J_AUTH" ]; then
   fi
 fi
 
-sed -i "s|#org.neo4j.server.webserver.address=0.0.0.0|org.neo4j.server.webserver.address=$HOSTNAME|g" $NEO4J_HOME/conf/neo4j-server.properties
+sed -i "s|#org.neo4j.server.webserver.address=0.0.0.0|org.neo4j.server.webserver.address=0.0.0.0|g" $NEO4J_HOME/conf/neo4j-server.properties
 
 # doing this conditionally in case there is already a limit higher than what
 # we're setting here. neo4j recommends at least 40000.
@@ -23,4 +24,4 @@ if [ "$limit" -lt 65536 ]; then
     ulimit -n 65536;
 fi
 
-.$NEO4J_HOME/bin/neo4j console
+exec .$NEO4J_HOME/bin/neo4j console
